068eb63ae59c84702ff777022a9fadcc9e23765f
eoin-kins <eoinkinsella@live.ie>
2024-06-29 16:46:21 +0100

    Home Page started and master css implimented

--- phys/src/components/Header.jsx
+++ phys/src/components/Header.jsx
@@ -11,7 +11,7 @@ function Header() {
           <Navbar.Brand href="/">Physics Soc</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="about">About</Nav.Link>
-            <Nav.Link href="gallery">Gallerry</Nav.Link>
+            <Nav.Link href="gallery">Gallery</Nav.Link>
             <Nav.Link href="newsletter">Newsletter</Nav.Link>
           </Nav>
         </Container>
--- phys/src/pages/Home.jsx
+++ phys/src/pages/Home.jsx
@@ -1,11 +1,25 @@
 import React from "react";
 import Header from "../components/Header";
-
+import "./Home.css";
+import "./Master.css";
 export const Home = () => {
   return (
     <>
       <Header />
-      <div>Home</div>
+      <img src="phys ban.png" className="banner" />
+      <div className="floater">
+        <h1>Our Mission</h1>
+        <p>
+          The Physics Society of University of Galway aims to join people
+          together through a love of Physics.
+        </p>
+        <p>All enthusiasts are welcome!</p>
+      </div>
+      <br></br>
+      <div className="floater">
+        <h1>Our Committee</h1>
+        <img src="phys com.PNG" />
+      </div>
     </>
   );
 };
